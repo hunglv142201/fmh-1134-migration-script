3e3b482373418af983053460648871fe5a79fdd2
Le Viet Hung <lvhu@netcompany.com>
2022-10-20 14:24:50 +0700

    Fixed config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const contentful = {
-    managementApiAccessToken: 'CFPAT-wiDoE69_-d20XU_5apSRBOd-UEbroeqbxyubnkKSLLU',
-    spaceId: 'wydyn9jcy2o3',
-    environment: 'lvhu-fmh-1016-bidirectional-linked-list'
+    managementApiAccessToken: 'TODO',
+    spaceId: 'TODO',
+    environment: 'TODO'
 };
